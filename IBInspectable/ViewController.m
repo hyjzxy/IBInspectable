22e4d1ef57d89f8b0f77d46681673c37033b8b31
Mingkui.liu <754925822@qq.com>
2016-03-20 15:16:30 +0800

    修改背景颜色

--- IBInspectable/ViewController.m
+++ IBInspectable/ViewController.m
@@ -18,7 +18,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+    self.view.backgroundColor = [UIColor whiteColor];
     
     // Do any additional setup after loading the view, typically from a nib.
 }
